820d561790efb23af368e0a553d17f5a7731383e
Jia Min <jiaminw@ymail.com>
2016-09-06 22:14:33 +0800

    Added RequiredWordsTest

---
+++ src/test/java/test/RequiredWordsTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import CS3213.RequiredWords;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RequiredWordsTest {
+
+    @Test
+    public void testGetSortedLines() throws Exception {
+        
+        RequiredWords requiredWords = new RequiredWords();
+
+        assertFalse(requiredWords.isWordRequired("Star"));
+        
+        String required = "Star, Space";
+        requiredWords.addRequiredWord(required);
+        
+        assertTrue(requiredWords.isWordRequired("Star"));
+        assertTrue(requiredWords.isWordRequired("Space"));
+        assertFalse(requiredWords.isWordRequired("Man"));
+        assertFalse(requiredWords.isWordRequired("Day"));
+
+        required = "Star, Day, Fast, Man";
+        requiredWords.addRequiredWord(required);
+        assertTrue(requiredWords.isWordRequired("Space"));
+        assertTrue(requiredWords.isWordRequired("Day"));
+        assertTrue(requiredWords.isWordRequired("Man"));
+        assertTrue(requiredWords.isWordRequired("Fast"));
+        assertFalse(requiredWords.isWordRequired("Star"));
+    }
+}
